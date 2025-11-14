7d1f68e8834f9c90725f6891548ade860973f5d7
trunghieu171007 <your_github_email@example.com>
2025-11-14 15:45:13 +0700

    update menu lab3

--- Lab3/lab3.c
+++ Lab3/lab3.c
@@ -1,84 +1,82 @@
 #include <stdio.h>
 #include <math.h>
 int main() {
-    // Bài 1: XÂY DỰNG CHƯƠNG TRÌNH TÍNH HỌC LỰC    
-    int diem, a, b, c;
-    double delta, nghiem1, nghiem2;
-    printf("nhap diem sinh vien:");
-    scanf("%d", &diem);
-    if (diem >= 9) {
-        printf("hoc luc xuat sac");
-    }
-    if (diem >= 8 && diem < 9) {
-        printf("hoc luc gioi");
-    }
-    if (diem >= 6.5 && diem < 8) {
-        printf("hoc luc trung binh");
-    } else {
-        printf("hoc luc yeu");
-    }
-
-    // BÀI 2: GIẢI PHƯƠNG TRÌNH BẬC NHẤT
-    printf("\nnhap gia tri a va b:");
-    scanf("%d%d", &a, &b);
-    if (a == 0) {
-        if (b == 0) {
-            printf("phuong trinh vo so nghiem");
-        } else {
-            printf("phuong trinh vo nghiem");
-        }
-    } else {
-        nghiem1 = -(double)b / a;
-        printf("nghiem cua phuong trinh la %f", nghiem1);
-    }
-
-    // switch case
-    printf("\nviet nam co bao nhieu dan toc anh em\n");
-    printf("a. 52\nb. 53\nc. 54\nd. 55\n");
-    printf("chon dap an abcd: ");
-    char dapan;
-    scanf(" %c", &dapan);
-    switch (dapan) {
-        case 'a':
-            printf("ban da chon sai");
-            break;
-        case 'b':
-            printf("ban da chon sai");
-            break;
-        case 'c':
-            printf("dap an chinh xac");
-            break;
-        case 'd':
-            printf("ban da chon sai");
-            break;
-        default:
-            printf("vui long chon lai");
-    }
-
-    // BÀI 3: GIẢI PHƯƠNG TÌNH BẬC 2
-    
-    printf("\nnhap gia tri a, b, c: ");
-    scanf("%d%d%d", &a, &b, &c);
-        if (a==0) {
-            nghiem1 = -(double)c/b;
-            printf("nghiem cua phuong trinh la: %lf", nghiem1);
-        }
-        else if (a!=0){
-            delta = (double)b*(double)b - 4.0*(double)a*(double)c;
-            if (delta<0) {
-                printf("phuong trinh vo nghiem");
+    int luachon;
+    do {
+        printf("menu chuong trinh vui long chon:");
+        printf("1. Tinh hoc luc");
+        printf("2. Giai phuong trinh bac nhat");
+        printf("3. Giai phuong trinh bac 2");
+        printf("0. Thoat chuong trinh");
+        printf("Nhạp lua chon cua ban:");
+        scanf("%d", &luachon);
+        
+        //Bài 1: Tính học lực sinh viên
+        switch (luachon) {
+            case 1: {
+                int diem;
+                printf("Hay nhap diem trung binh cua sinh vien:");
+                scanf("%d", &diem);
+                if (diem>=9)
+                    printf("hoc luc xuat sac");
+                else if (diem>=8)
+                    printf("hoc luc gioi");
+                else if (diem>=6.5)
+                    printf("hoc luc trung binh");
+                else
+                    printf("hoc luc yeu");
+                break;
             }
-            else if (delta == 0) {
-                nghiem1 = -(double)b/(2*a);
-                printf("phuong trinh co nghiem kep la: %f", nghiem1);
+
+        //Bài 2: Giải phương trình bậc nhất
+            case 2: {
+                int a, b;
+                double nghiem;
+                printf("hay nhap gia tri a va b");
+                scanf("%d%d", &a, &b);
+                if (a==0) {
+                    if (b==0)
+                        printf("phuong trinh vo so nghiem");
+                    else
+                        printf("phuong trinh vo nghiem");
+                } else {
+                    nghiem = -(double)b/a;
+                    printf("nghiem cua phuong trinh la:%f", nghiem);
+                }
+                break;
             }
-            else {
-                printf("phuong trinh co 2 nghiem rieng biet la:");
-                nghiem1 = (-b + sqrt(delta))/(2*a);
-                printf("\n%f", nghiem1);
-                nghiem2 = (-b - sqrt(delta))/(2*a);
-                printf("\n%f", nghiem2);
+
+        //Bài 3:Giải phương trình bậc ba
+            case 3: {
+                int a, b, c;
+                double delta, nghiem1, nghiem2;
+                printf("nhap gia tri a, b, c,d:");
+                scanf("%d%d%d", &a, &b, &c);
+                if (a==0) {
+                    nghiem1 = -(double)c/b;
+                    printf("nghiem cua phuong trinnh la:%f", nghiem1);
+                } else {
+                    delta = (double)b*b - 4.0*a*c;
+                    if (delta <0) {
+                        printf("phương trình vô nghiệm:");
+                    }
+                    else if (delta==0) {
+                        nghiem1 = -(double)b/(2*a);
+                        printf("phuong trinh co nghiem kep:%f", nghiem1);
+                    } else {
+                        nghiem1 = (-b + sqrt(delta)/(2*a));
+                        nghiem2 = (-b - sqrt(delta)/(2*a));
+                        printf("Phuong trinh co 2 nghiem phan biet la %f va %f", nghiem1, nghiem2);
+                    }
+                }
+                break;
             }
+            case 0:
+                printf("ket thuc chuong trinh");
+                break;
+            default:
+                printf("vui long chon lai");
         }
+    } while(luachon !=0);
     return 0;
 }
